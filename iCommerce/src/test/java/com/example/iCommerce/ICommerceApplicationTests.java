3d769b643889105e708fba3b630da20461405fac
PHAM GIA HUY <phamgiahuytkd@gmail.com>
2024-10-27 20:08:16 +0700

    Start

---
+++ iCommerce/src/test/java/com/example/iCommerce/ICommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ICommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
