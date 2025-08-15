8d75163fa67e599d7b0982befaf99344507c28f0
PHAM GIA HUY <phamgiahuytkd@gmail.com>
2025-08-15 16:37:35 +0700

    package v4.7

--- src/main/java/com/example/iCommerce/client/MomoApi.java
+++ src/main/java/com/example/iCommerce/client/MomoApi.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "momo", url = "${momo.end-point}")
+@FeignClient(name = "momo", url = "https://test-payment.momo.vn/v2/gateway/api/create")
 public interface MomoApi {
     @PostMapping
 //            ("/create")
