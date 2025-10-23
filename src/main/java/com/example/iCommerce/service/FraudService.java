8c37f34da26494b46c13f11d3db489c381f044db
PHAM GIA HUY <phamgiahuytkd@gmail.com>
2025-10-23 17:28:17 +0700

    v7.96

--- src/main/java/com/example/iCommerce/service/FraudService.java
+++ src/main/java/com/example/iCommerce/service/FraudService.java
@@ -61,10 +61,10 @@ public class FraudService {
         // ⚙️ Quy đổi fraudScore (0–1) sang phần trăm (0–100)
         assert response != null;
         double fraudScorePercent = response.getProbability();
-        double reputation = user.getReputation();
+        double reputation = 100-user.getReputation();
 
         // ✅ Tính điểm tổng hợp: 70% fraud score + 30% reputation
-        double finalScore = fraudScorePercent * 0.7 + reputation * 0.003 ;
+        double finalScore = (fraudScorePercent * 0.7) + reputation * 0.003 ;
 
         // ✅ Gán lại vào response (để frontend hiển thị)
         response.setProbability(finalScore);
